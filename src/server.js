1f539ce915425d6336f52cc406db209951e9b9d1
Bertha Janeth Pérez Canales <janeth_2124@hotmail.com>
2023-09-07 14:47:44 -0600

    MongoDB Connection
    The provided code utilizes the connect function from mongoose to connect to the database and then logs a message to the console indicating whether the connection was successful or if an error occurred.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config()
 
 const server = express(); 
 const port = process.env.PORT || 3000;
@@ -8,6 +10,11 @@ server.get('/', (req, res) => {
         res.send("welcome to my app")
 })
 
+//coneccion
+mongoose
+    .connect(process.env.MONGODB)
+    .then(() => console.log("conectado a mongodb"))
+    .catch((error) => console.error(error))
 
 server.listen(port, () => {
     console.log(`server listing on port ${port}`)
