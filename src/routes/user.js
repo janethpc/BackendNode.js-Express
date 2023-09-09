96a25d80258d7dabf44778e49c2735b1875877e1
Bertha Janeth Pérez Canales <janeth_2124@hotmail.com>
2023-09-08 19:43:32 -0600

    Added API routes for reading, updating, and deleting user information
    In this commit, I've implemented three API routes to interact with user data. These routes allow you to:
    1. Read a single user's information by their ID.
    2. Update user details such as name, age, and email by their ID.
    3. Delete a user by their ID.
    
    Each route includes error handling to respond with appropriate messages in case of user not found or any other errors.

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ import userSchema from '../models/user.js';
 
 const router = Router();
 
-
+// crear un nuevo usuario
 router.post('/users', async (req, res) => {
     try{
         const user = new userSchema(req.body);
@@ -12,8 +12,9 @@ router.post('/users', async (req, res) => {
     }catch(error){
         res.status(500).json({message: error.message})
     }
-})
+});
 
+// lee todos los usuarios
 router.get('/users', async (req, res) => {
     try{
         const users = await userSchema.find()
@@ -21,6 +22,52 @@ router.get('/users', async (req, res) => {
     }catch(error){ 
         res.status(500).json({message: error.message})
     }
-})
+}); 
+
+// lee un solo usuario 
+router.get('/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const user = await userSchema.findById(id);
+
+
+        if(!user){
+            return res.status(500).json({message: 'User not found'})
+        }res.json(user)
+    }catch(error){ 
+        res.status(500).json({message: error.message})
+    }
+});
+
+// editar usuario 
+router.put('/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {name, age, email} = req.body;
+
+        const user = await userSchema.findByIdAndUpdate(id, {name, age, email}, {new: true});
+
+        if(!user){
+            return res.status(500).json({message: 'User not found'})
+        }res.json(user)
+    } catch (error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+//elinar usuario 
+router.delete('/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const user = await userSchema.findByIdAndRemove(id);
+
+        if(!user){
+            return res.status(500).json({message: 'User not found'})
+        }res.json({message: 'User deleted successfully'})
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
 
 export default router;
