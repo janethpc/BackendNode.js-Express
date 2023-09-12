cdabce9e79b963cc0feac67dc7289c464447e711
Bertha Janeth Pérez Canales <janeth_2124@hotmail.com>
2023-09-12 13:35:33 -0600

    Update AdminProfile.jsx

--- client/src/pages/AdminProfile.jsx
+++ client/src/pages/AdminProfile.jsx
@@ -7,8 +7,14 @@ const AdminProfile = () => {
     < >
     <NavBarAdmin/>
     <div className="container text-center" id="alingCenter">
+      <div className="row row-cols-2 align-items-center justify-content-center">
+        <div className="col-4">
         <img src={adminPage} alt="admin" />
+        </div>
+        <div className="col-4">
         <h1> Welcome Admin </h1>
+        </div>
+        </div>
     </div>
     </>
   )
